1b165dacf7e56d189723d70a691f2a633633b70e
Lyanair <Rloughran280@gmail.com>
2024-11-13 15:51:05 +0000

    13/11/2024

--- SpaceGame/Assets/Scripts/Player movement.cs
+++ SpaceGame/Assets/Scripts/Player movement.cs
@@ -8,17 +8,23 @@ public class Playermovement : MonoBehaviour
     public float movespeed = 5f;
     
     Vector2 Player_move;
+    Vector2 Mouse_point;
     void Update()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         Player_move = new Vector2(moveX, moveY).normalized;
+        Mouse_point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
         body.velocity = new Vector2(Player_move.x * movespeed, Player_move.y * movespeed);
+
+        Vector2 aim = Mouse_point - Player_move.position;
+        float aimangle = Mathf.Atan2(aim.y,aim.x) * Mathf.Rad2Deg - 90f;
+        body.rotation = aimangle;
     }
 }
 
