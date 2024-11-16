31238eb7392df4d371b2033c072ec5a432e399e1
Titon-TGC <titontgc@gmail.com>
2024-11-16 01:50:28 +0000

    temp

---
+++ SpaceGame/Assets/Scripts/TransitionLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionLoader : MonoBehaviour
+{
+    public Animator transition;
+    public string scene;
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(scene);
+    }
+}
