d4b3cbd273c22ced56ec642daf86df00c3d7622e
Lyanair <Rloughran280@gmail.com>
2024-11-16 21:56:41 +0000

    bug fixes

--- SpaceGame/Assets/Scripts/Enemy/Enemy_Attack.cs
+++ SpaceGame/Assets/Scripts/Enemy/Enemy_Attack.cs
@@ -5,18 +5,31 @@ using UnityEngine;
 public class Enemy_Attack : MonoBehaviour
 {
     public int damageAmount;
+    private float attackHeat;
+    private const float timeBetweenAttacks = 0.5f;
+
+    void Update()
+    {
+        if (attackHeat > 0)
+        {
+            attackHeat -= Time.deltaTime;
+        }
+
+    }
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<Playermovement>())
+        if (collision.gameObject.GetComponent<Playermovement>() && attackHeat <= 0)
         {
             var healthController = collision.gameObject.GetComponent<Health>();
             healthController.TakeDamage(damageAmount);
+            attackHeat = timeBetweenAttacks;
         }
-        if (collision.gameObject.GetComponent<Shiphealth>())
+        if (collision.gameObject.GetComponent<Shiphealth>() && attackHeat <= 0)
         {
             var healthController = collision.gameObject.GetComponent<Shiphealth>();
             healthController.ShipDamage(damageAmount);
+            attackHeat = timeBetweenAttacks;
         }
     }
 }
